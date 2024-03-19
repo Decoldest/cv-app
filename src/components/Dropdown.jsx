dc166ce42e586560aa1f8176345d60eeb41d2c17
Decoldest <127795154+Decoldest@users.noreply.github.com>
2024-03-19 10:06:17 -0400

    Create react project

---
+++ src/components/Dropdown.jsx
@@ -0,0 +1,10 @@
+export default function Dropdown() {
+  return (
+    <div>
+      <button>
+        show
+      </button>
+      
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Dropdown from './components/Dropdown'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Dropdown></Dropdown>
+  </React.StrictMode>,
+)
